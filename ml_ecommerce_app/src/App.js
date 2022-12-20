bf9daf3aa1d8db25d1b4211d6cd31991adda9179
Cristina SG <cristinasanchezg@gmail.com>
2022-12-20 17:50:54 -0600

    Importar componentes

--- ml_ecommerce_app/src/App.js
+++ ml_ecommerce_app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import AppRouter from './routes/AppRouter';
 import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <AppRouter />
+      <Footer />
     </BrowserRouter>
   );
 }
