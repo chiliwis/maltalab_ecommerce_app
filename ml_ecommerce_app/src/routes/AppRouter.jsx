913317ef8bc40a80df5461d10bbe264f22957fbc
Cristina SG <cristinasanchezg@gmail.com>
2022-12-20 18:46:42 -0600

    footer

--- ml_ecommerce_app/src/routes/AppRouter.jsx
+++ ml_ecommerce_app/src/routes/AppRouter.jsx
@@ -9,22 +9,23 @@ import Signup from '../pages/Auth/Signup';
 import ProductDetail from '../pages/products/ProductDetail';
 import ProductList from '../pages/products/ProductLists';
 
-function AppRouter () {
-    <Routes>
-        <Route path="/home" element={<Home/>} />
-        <Route path="/checkout" element={<Checkout/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/products" element={<ProductList/>} />
-        <Route path="/detail-product" element={<ProductDetail/>} />
+function AppRouter() {
+    return (
+        <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/products" element={<ProductList />} />
+            <Route path="/detail-product" element={<ProductDetail />} />
 
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/404" element={<NotFound/>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/404" element={<NotFound />} />
 
-        <Route path="*" element={<Navigate to="404" />} />
+            <Route path="*" element={<Navigate to="404" />} />
 
-    </Routes>
-
+        </Routes>
+    )
 }
 
 export default AppRouter;
