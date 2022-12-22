50f4e05b00c39c31fade4e369592278cf55ec57c
Cristina SG <cristinasanchezg@gmail.com>
2022-12-21 18:50:01 -0600

    header y footer

--- ml_ecommerce_app/src/components/Navbar.jsx
+++ ml_ecommerce_app/src/components/Navbar.jsx
@@ -1,6 +1,28 @@
-function Navbar() {
+import { Link, NavLink} from 'react-router-dom'
+
+const Navbar = () => {
+    function menuResponsive() {
+
+    };
+
     return ( 
-        <h1>Componente Navbar</h1>
+        <header className="site-header sticky-top">
+            <div className="topnav" id="myTopnav">
+                <Link to="/home"> CafeMX</Link>
+                <NavLink to="/home">Productos</NavLink>
+                <NavLink to="/checkout">Carrito</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/login">Login</NavLink>
+                <NavLink to="/signup">Signup</NavLink>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a className="icon" onClick={menuResponsive}>
+                    <i className="fa fa-bars" />
+                </a>
+            </div>
+
+
+        </header>
+
      );
 }
 
